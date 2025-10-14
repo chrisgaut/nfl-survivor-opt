1eddf8885cd26ffeab49753221793d1af1e1d9c3
Christopher Gauthier <54082623+chrisgaut@users.noreply.github.com>
2025-10-13 19:15:33 -0700

    survivor path visualization updates

--- 03-viz-results.R
+++ 03-viz-results.R
@@ -1,6 +1,13 @@
 
 # Script 03: Visualize results
 
+# TODO:
+# - Note at bottom for date in which script was ran
+# - General visualization improvements
+# - Another visualization that shows the entire table, and the path highlighted
+# - Team logos for survivor list
+# - Weighted graph visualization + project description for README
+
 
 
 
@@ -15,54 +22,52 @@ library(gt)
 
 
 
-# 2. Table Formatting ----------------------------------------------------------
+# 2. Survivor Path Formatting --------------------------------------------------
 
 # combine made picks and remaining survivor path
-survivor_path <- survivor_path %>%
-  mutate(result = NA)
+survivor_path <- survivor_df %>%
+  mutate(result = "")
 
-survivor_table <- survivor_path %>%
+survivor_path <- survivor_path %>%
   rbind(picks) %>%
   mutate(week = as.numeric(week)) %>%
   arrange(week)
 
 # bring in game information
-survivor_table <- survivor_table %>%
+survivor_path <- survivor_path %>%
   left_join(nfl_games, by = c("week", "team"))
 
 # bring in strength for survivor path
-survivor_table <- survivor_table %>%
-  left_join(select(game_set, week, team, strength), by = c("week", "team"))
-
-# reorganize columns
-survivor_table <- survivor_table %>%
-  select(week, team, opponent, strength)
+survivor_path <- survivor_path %>%
+  left_join(select(game_set, week, team, strength), by = c("week", "team")) %>%
+  mutate(strength = replace_na(strength, ""))
 
 
 
 
 
-
-
-# 3. Survivor Table Graphic ----------------------------------------------------
+# 3. Survivor Path Graphic -----------------------------------------------------
 
 # table
-gt_survivor_tbl <- gt(survivor_table) %>%
+gt_survivor_tbl <- gt(survivor_path) %>%
   tab_header(
-    title = md("**NFL Survivor Table**"),
+    title = md("**NFL Survivor Path**"),
     subtitle = "The optimal remaining path to survival"
     ) %>%
   tab_source_note(
-    source_note = md("*Strength* is defined as difference between the 
-                     opponent's and team's PFF current power ranking")
+    source_note = md("*Strength* is defined as the difference between the<br> 
+                     opponent's and team's current PFF power rankings.")
     ) %>%
   tab_style(
     style = cell_fill(color = "#009900"),
     locations = cells_body(
       rows =  week <= week_completed
     )
-    )
+    ) %>%
+  cols_hide(c(home, result))
 
 
 
 
+
+# 4. Survivor Table Graphic ----------------------------------------------------
