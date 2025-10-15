d37c2d1e84d0b45a7df5a9632fb317e16701b37d
Christopher Gauthier <54082623+chrisgaut@users.noreply.github.com>
2025-10-14 21:22:27 -0700

    Survivor table graphic instantiation

--- 03-viz-results.R
+++ 03-viz-results.R
@@ -4,7 +4,6 @@
 # TODO:
 # - Note at bottom for date in which script was ran
 # - General visualization improvements
-# - Another visualization that shows the entire table, and the path highlighted
 # - Team logos for survivor list
 # - Weighted graph visualization + project description for README
 
@@ -49,7 +48,7 @@ survivor_path <- survivor_path %>%
 # 3. Survivor Path Graphic -----------------------------------------------------
 
 # table
-gt_survivor_tbl <- gt(survivor_path) %>%
+gt_survivor_path <- gt(survivor_path) %>%
   tab_header(
     title = md("**NFL Survivor Path**"),
     subtitle = "The optimal remaining path to survival"
@@ -70,4 +69,35 @@ gt_survivor_tbl <- gt(survivor_path) %>%
 
 
 
-# 4. Survivor Table Graphic ----------------------------------------------------
+# 4. Survivor Table Formatting -------------------------------------------------
+
+# create pivot table
+survivor_table <- game_set %>%
+  select(team, pff_rank_team, week, opponent) %>%
+  pivot_wider(names_from = week, values_from = opponent)
+
+# rename and reorder columns
+week_cols <- c("7", "8", "9", "10", "11", "12"
+               , "13", "14", "15", "16", "17", "18")
+colnames(survivor_table)[colnames(survivor_table) %in% week_cols] <- 
+  paste0("Week ", colnames(survivor_table)[colnames(survivor_table) %in% week_cols])
+
+survivor_table <- survivor_table %>%
+  select(2, 1, 3, 14, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13) %>%
+  replace(is.na(.), "BYE")
+
+
+
+
+# 5. Survivor Table Graphic ----------------------------------------------------
+
+# table
+gt_survivor_table <- gt(survivor_table) %>%
+  tab_header(
+    title = md("**NFL Survivor Table**"),
+    subtitle = "The optimal remaining path to survival"
+  )
+
+
+
+
